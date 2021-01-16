9a95931ddb42fdc1e266767e757c65215c3cb7a1
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 13:55:51 +0100

    Exercice 4

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 50);
+            int response = random.Next(0,50);
+            int attempt = 1;
+            for (int i = 1; i < 50; i++)
+            while (response != random.Next(0,50))
+            {
+                Console.WriteLine("Devine le nombre secret entre 0 et 50");
+                response = int.Parse(Console.ReadLine());
+                if (response < number)
+                {
+                    Console.WriteLine("Non c'est plus grand");
+                    Console.WriteLine("Tentative n° " + attempt);
+                        ++attempt;
+                }
+                else if (response > number)
+                {
+                    Console.WriteLine("Non c'est plus petit");
+                    Console.WriteLine("Tentative n° " + attempt);
+                        ++attempt;
+                }
+                else if (response == number)
+                {
+                    Console.WriteLine("Bravo, vous avez trouvé !");
+                }
+            }
+        }
+    }
+}
